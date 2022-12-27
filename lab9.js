783187dc536221f61f743366b1efc7b18b8ae22a
Blyk <egor_b@ngs.ru>
2022-12-27 09:32:08 +0700

    Кнопка Имя-Фамилия

--- lab9.js
+++ lab9.js
@@ -4,3 +4,15 @@ function clickMe(button) {
     else
         button.innerHTML = 'Щелчок';
 }
+function nameegor(button) {
+    if(button.innerHTML == 'Егор')
+        button.innerHTML = 'Блык';
+    else
+    button.innerHTML = 'Егор'
+}
+function namevlad(button) {
+    if(button.innerHTML == 'Влад')
+        button.innerHTML = 'Щеглов';
+    else
+    button.innerHTML = 'Влад'
+}
